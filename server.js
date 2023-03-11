43ea79e2e5d29b972f4dd36eb0b477778ffe1392
Kevin Compton <kevincomptondev@gmail.com>
2023-03-11 15:40:08 -0500

    Added functionality to work with horoku

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const methodOverride = require("method-override");
 
 // DATABASE CONNECTION
 // How to connect to the database either via heroku or locally
-
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.set('strictQuery', true)
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: false,
+})
 
 
 // Mongo error/success
@@ -26,6 +30,7 @@ app.use(methodOverride("_method"));
 app.use(express.static('public'));
 
 // ROUTES - INDUCES
+
 
 //HOME
 app.get('/', (req, res)=>{
